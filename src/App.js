771ef45f45b8c0463eb02ceaa0aa271745231777
isakashiori <isslamdunk0000@gmail.com>
2022-12-31 20:23:56 +0900

    add: ルーティングの設定

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { createHashHistory } from '@remix-run/router'
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import About from "./screens/About";
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   )
 }
-
 
 export default App
